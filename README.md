843e61a72dd0df29427ca8b652c7325d0df59804
Vladyslav Pekker <agilesteel@gmail.com>
2019-10-17 14:28:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Installs [Byobu](http://byobu.co/) on [Ubuntu](https://www.ubuntu.com/)
+
+```bash
+wget -q https://script.install.devinsideyou.com/byobu
+sudo chmod +x byobu && ./byobu
+```
---
+++ install-byobu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# remove yourself
+rm $0
+
+# install byobu and adds (might cause duplicates) it to the list of shells
+sudo apt install -y byobu && which byobu | sudo tee -a /etc/shells
+
+echo
+echo '"byobu" is now on the path'
