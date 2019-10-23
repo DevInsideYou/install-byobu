e379de3f6d5679ae12af062847a8b9c877fa2d7a
Vladyslav Pekker <agilesteel@gmail.com>
2019-10-23 20:16:38 +0200

    Update install-byobu.sh

--- install-byobu.sh
+++ install-byobu.sh
@@ -3,6 +3,9 @@
 # remove yourself
 rm $0
 
+# install xclip for copying into the clipboard
+sudo apt-get install -yqqq xclip
+
 # install byobu and add (might cause duplicates) it to the list of shells
 sudo apt install -y byobu && which byobu | sudo tee -a /etc/shells
 
